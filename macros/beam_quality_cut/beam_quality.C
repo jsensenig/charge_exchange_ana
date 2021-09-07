7f0c66c423629019a5646b556e95129517a2b3fc
jsensenig <jonsensenig17@gmail.com>
2021-09-07 11:06:46 -0400

    Cleanup the beam quality cut fitting macro

--- macros/beam_quality_cut/beam_quality.C
+++ macros/beam_quality_cut/beam_quality.C
@@ -1,7 +1,27 @@
 
+void fit_hist( const std::string &name, TH1D* hist ) {
+
+  // Scale the histogram integrals to 1
+  hist->Scale( 1. / hist->Integral() );
+
+  TCanvas c = TCanvas();
+  gStyle->SetOptFit(0x2);
+
+  // Fit beam start position or angle
+  TF1 f("f", "gaus");
+  hist->Fit("f");
+  hist->Draw();
+
+  // .. and save to a .pdf
+  std::string pdf_name = "beam_" + name + "_fit.pdf";
+  c.SaveAs(pdf_name.c_str());
+
+}
+
 void beam_quality() {
 
   std::string fname = "/Users/jsen/tmp/pion_qe/ana_scripts/merge_files/output.root";
+  std::string treename = "beamana;17";
 
   TFile *file_address = TFile::Open(fname.c_str());
 
@@ -10,8 +30,7 @@ void beam_quality() {
     return;
   }
 
-  TTree *tree = (TTree*)file_address->Get("beamana;17");
-
+  TTree *tree = (TTree*)file_address->Get(treename.c_str());
 
   double reco_beam_calo_startX, reco_beam_calo_startY, reco_beam_calo_startZ;
   double reco_beam_calo_endX, reco_beam_calo_endY, reco_beam_calo_endZ;
@@ -23,23 +42,25 @@ void beam_quality() {
   tree->SetBranchAddress("reco_beam_calo_endY", &reco_beam_calo_endY);
   tree->SetBranchAddress("reco_beam_calo_endZ", &reco_beam_calo_endZ);
 
-  TH1D hx("hx", "Beam Calo Start X;X [cm];Count", 50, -80., 20.);
-  TH1D hy("hy", "Beam Calo Start Y;Y [cm];Count", 40, 350., 500.);
-  TH1D hz("hz", "Beam Calo Start Z;Z [cm];Count", 25, -5., 10.);
+  std::map<std::string, TH1D*> hist_map;
 
-  TH1D thetax("thetax", "Beam Angle #theta_x;#theta_x [deg];Count", 60, 0.0, 180.0);
-  TH1D thetay("thetay", "Beam Angle #theta_y;#theta_y [deg];Count", 60, 0.0, 180.0);
-  TH1D thetaz("thetaz", "Beam Angle #theta_z;#theta_z [deg];Count", 30, 0.0, 90.0);
+  TH1D* hx = hist_map["startx"] = new TH1D("hx", "Beam Calo Start X;X [cm];Count", 50, -80., 20.);
+  TH1D* hy = hist_map["starty"] = new TH1D("hy", "Beam Calo Start Y;Y [cm];Count", 40, 350., 500.);
+  TH1D* hz = hist_map["startz"] = new TH1D("hz", "Beam Calo Start Z;Z [cm];Count", 25, -5., 10.);
 
+  TH1D* thetax = hist_map["anglex"] = new TH1D("thetax", "Beam Angle #theta_x;#theta_x [deg];Count", 60, 0.0, 180.0);
+  TH1D* thetay = hist_map["angley"] = new TH1D("thetay", "Beam Angle #theta_y;#theta_y [deg];Count", 60, 0.0, 180.0);
+  TH1D* thetaz = hist_map["anglez"] = new TH1D("thetaz", "Beam Angle #theta_z;#theta_z [deg];Count", 30, 0.0, 90.0); 
+  
   size_t nevts = tree -> GetEntries();
 
   for ( size_t evt = 0; evt < nevts; evt++ ) {
     tree->GetEntry( evt );
 
     // Fill the beam x,y,x
-    hx.Fill( reco_beam_calo_startX );
-    hy.Fill( reco_beam_calo_startY );
-    hz.Fill( reco_beam_calo_startZ );
+    hx->Fill( reco_beam_calo_startX );
+    hy->Fill( reco_beam_calo_startY );
+    hz->Fill( reco_beam_calo_startZ );
 
     // Beam angles
     TVector3 beam_start(reco_beam_calo_startX, reco_beam_calo_startY, reco_beam_calo_startZ);
@@ -52,55 +73,13 @@ void beam_quality() {
 
     // theta_i = arccos( i * cos(theta_i) )  where i = {x,y,z}
     // practically: theta_i = arccos( beam_dir.i() ) where  i={x,y,z} and beam_dir normalized
-    thetax.Fill( TMath::RadToDeg() * TMath::ACos(beam_dir.X()) );
-    thetay.Fill( TMath::RadToDeg() * TMath::ACos(beam_dir.Y()) );
-    thetaz.Fill( TMath::RadToDeg() * TMath::ACos(beam_dir.Z()) );
+    thetax->Fill( TMath::RadToDeg() * TMath::ACos(beam_dir.X()) );
+    thetay->Fill( TMath::RadToDeg() * TMath::ACos(beam_dir.Y()) );
+    thetaz->Fill( TMath::RadToDeg() * TMath::ACos(beam_dir.Z()) );
 
   }
 
-  // Scale the histogram integrals to 1
-  hx.Scale( 1. / hx.Integral() );
-  hy.Scale( 1. / hy.Integral() );
-  hz.Scale( 1. / hz.Integral() );
-
-  thetax.Scale( 1. / thetax.Integral() );
-  thetay.Scale( 1. / thetay.Integral() );
-  thetaz.Scale( 1. / thetaz.Integral() );
-
-
-  TCanvas c = TCanvas();
-  gStyle->SetOptFit(0x2);
-
-  // Fit beam start position
-  TF1 fx("fx", "gaus", -80., 20.);
-  hx.Fit("fx", "R");
-  hx.Draw();
-  c.SaveAs("beam_start_x_fit.pdf");
-
-  TF1 fy("fy", "gaus", 350., 500.);
-  hy.Fit("fy", "R");
-  hy.Draw();
-  c.SaveAs("beam_start_y_fit.pdf");
-
-  TF1 fz("fz", "gaus", -5., 10.);
-  hz.Fit("fz", "R");
-  hz.Draw();
-  c.SaveAs("beam_start_z_fit.pdf");
-
-  // Fit beam direction x,y,z angle
-  TF1 fthetax("fthetax", "gaus", 0., 180.);
-  thetax.Fit("fthetax", "R");
-  thetax.Draw();
-  c.SaveAs("beam_theta_x_fit.pdf");
-                                    
-  TF1 fthetay("fthetay", "gaus", 0., 180.);
-  thetay.Fit("fthetay", "R");
-  thetay.Draw();
-  c.SaveAs("beam_theta_y_fit.pdf");
-                                    
-  TF1 fthetaz("fthetaz", "gaus", 0., 90.);
-  thetaz.Fit("fthetaz", "R");
-  thetaz.Draw();
-  c.SaveAs("beam_theta_z_fit.pdf");
+  // Normalize, fit and save the histograms
+  for( auto &map : hist_map ) fit_hist( map.first, map.second );
 
 }

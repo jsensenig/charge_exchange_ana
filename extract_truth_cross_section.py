2975d43fe068a69102c30d10d5756d9615eadae4
jsensenig <jonsensenig17@gmail.com>
2022-01-15 09:50:59 -0500

    Add script to run cross as a standalone process

---
+++ extract_truth_cross_section.py
@@ -0,0 +1,23 @@
+from cross_section.cex_dd_cross_section import CexDDCrossSection
+import uproot
+
+
+branches = ["event", "reco_daughter_PFP_true_byHits_startZ", "reco_daughter_PFP_true_byHits_PDG", "reco_beam_passes_beam_cuts",
+            "reco_daughter_PFP_true_byHits_parPDG", "true_beam_daughter_startP", "true_beam_daughter_PDG",
+            "reco_beam_true_byHits_PDG", "reco_daughter_allShower_energy", "reco_daughter_PFP_trackScore_collection",
+            "reco_daughter_allTrack_Chi2_proton", "reco_daughter_allTrack_Chi2_ndof",
+            "true_daughter_nPiMinus", "true_daughter_nPiPlus", "true_daughter_nPi0", "true_daughter_nProton",
+            "true_daughter_nNeutron","true_beam_PDG","true_beam_endProcess","true_beam_endZ","true_beam_endP","true_beam_endPx",
+            "true_beam_endPy", "true_beam_endPz", "reco_daughter_PFP_michelScore_collection", "reco_beam_calo_startX",
+            "reco_beam_calo_startY", "reco_beam_calo_startZ", "reco_beam_calo_endX", "reco_beam_calo_endY",
+            "reco_beam_calo_endZ", "true_beam_daughter_startPx", "true_beam_daughter_startPy", "true_beam_daughter_startPz",
+            "reco_beam_true_byHits_endProcess", "reco_daughter_PFP_nHits", "reco_beam_vertex_michel_score",
+            "reco_beam_vertex_nHits"]
+
+# Single beam pi+ events
+files = "/Users/jsen/tmp/pion_qe/2gev_single_particle_sample/v0_limited_daughter/pduneana*.root"
+
+all_events = uproot.concatenate(files={files:"pduneana/beamana;1"}, expressions=branches)
+xsec = CexDDCrossSection(None)
+xsec.extract_cross_section(all_events=all_events, selected_events=all_events, total_incident_pion=14000)
+

656e8846016617ac7f1cdc5aff5f9211f0f34895
jsensenig <jonsensenig17@gmail.com>
2022-02-07 15:42:16 -0500

    Add ratio plot and plot legend

--- macros/data_mc_comparison/README.md
+++ macros/data_mc_comparison/README.md
@@ -1,6 +1,6 @@
 To run use
-```python
-python data_mc_plots.py <Input MC ROOT File> <Input Data ROOT File> <Output Dir>
+```bash
+python data_mc_plots.py <Input_MC_ROOT_File> <Input_Data_ROOT_File> <Output_Dir>
 ```
 
 Requires output ROOT file format from selection step.
--- macros/data_mc_comparison/data_mc_plots.py
+++ macros/data_mc_comparison/data_mc_plots.py
@@ -2,7 +2,60 @@ import ROOT
 import sys
 
 
-def scale_and_plot(mc_hist_dict, data_hist_dict, mc_evts, data_evts, outdir):
+def create_ratio(h1, h2):
+    h3 = h1.Clone("h3")
+    h3.SetLineColor(1)
+    h3.SetMarkerStyle(20)
+    h3.SetMarkerSize(0.6)
+    h3.SetTitle("")
+    h3.SetMinimum(0.5)
+    h3.SetMaximum(1.5)
+    # Set up plot for markers and errors
+    h3.Sumw2()
+    h3.SetStats(0)
+    h3.Divide(h2)
+
+    # Adjust y-axis settings
+    y = h3.GetYaxis()
+    y.SetTitle("Ratio MC/Data ")
+    y.SetNdivisions(505)
+    y.SetTitleSize(20)
+    y.SetTitleFont(43)
+    y.SetTitleOffset(1.55)
+    y.SetLabelFont(43)
+    y.SetLabelSize(15)
+
+    # Adjust x-axis settings
+    x = h3.GetXaxis()
+    x.SetTitleSize(20)
+    x.SetTitleFont(43)
+    x.SetTitleOffset(0.8)
+    x.SetLabelFont(43)
+    x.SetLabelSize(15)
+    x.SetTitle(h1.GetXaxis().GetTitle())
+
+    return h3
+
+
+def create_canvas_pads():
+    c = ROOT.TCanvas("c", "canvas", 800, 800)
+
+    # Upper histogram plot is pad1
+    pad1 = ROOT.TPad("pad1", "pad1", 0, 0.3, 1, 1.0)
+    pad1.SetBottomMargin(0)  # joins upper and lower plot
+    pad1.Draw()
+
+    # Lower ratio plot is pad2
+    c.cd()  # returns to main canvas before defining pad2
+    pad2 = ROOT.TPad("pad2", "pad2", 0, 0.05, 1, 0.3)
+    pad2.SetTopMargin(0)  # joins upper and lower plot
+    pad2.SetBottomMargin(0.2)
+    pad2.Draw()
+
+    return c, pad1, pad2
+
+
+def scale_and_plot(mc_hist_dict, data_hist_dict, legend_dict, mc_evts, data_evts, outdir):
     """
     Scale the MC to data, i.e. num_data_events/num_mc_events
     :param mc_hist_dict: Dict of MC histograms to plot
@@ -12,18 +65,33 @@ def scale_and_plot(mc_hist_dict, data_hist_dict, mc_evts, data_evts, outdir):
     :return:
     """
     for hmc in mc_hist_dict:
-        c = ROOT.TCanvas()
         # Get each of the MC histograms in the THStack and scale them
         stack_hists = mc_hist_dict[hmc].GetHists()
         for h in stack_hists:
             h.Scale(data_evts / mc_evts)
         mc_hist_dict[hmc].Modified()
+
+        # Create the ratio plot histogram
+        mc_hist = ROOT.TH1D(mc_hist_dict[hmc].GetStack().Last())
+        ratio_hist = create_ratio(mc_hist, data_hist_dict[hmc])
+        c, pad1, pad2 = create_canvas_pads()
+
         # Draw and save everything
+        pad1.cd()
         mc_hist_dict[hmc].Draw("HIST")
         data_hist_dict[hmc].SetMarkerColor(1)
         data_hist_dict[hmc].SetMarkerStyle(20)
         data_hist_dict[hmc].SetMarkerSize(0.6)
+        data_hist_dict[hmc].SetLineColor(1)
         data_hist_dict[hmc].Draw("SAME;E0")
+
+        # Draw the legend
+        legend_dict[hmc].AddEntry(data_hist_dict[hmc], "Data")
+        legend_dict[hmc].Draw("SAME")
+
+        pad2.cd()
+        ratio_hist.Draw("SAME;E1")
+
         c.Draw()
         c.Write(hmc)
         c.SaveAs(outdir + "/" + hmc + ".png")
@@ -41,6 +109,7 @@ def get_mc_data_histograms(mcfile, datafile):
 
     mc_hist_name_dict = {}
     data_hist_name_dict = {}
+    legend_dict = {}
 
     for hist in hist_list:
         h_name = hist.GetName()
@@ -51,11 +120,15 @@ def get_mc_data_histograms(mcfile, datafile):
         # Obviously Data doesn't have stacked histograms as this relies on truth info
         data_name = "hist" + h_name[5:]
         data_hist_name_dict[h_name] = datafile.Get(data_name)
+        legend_name = "legend_" + h_name
+        legend_dict[h_name] = mcfile.Get(legend_name)
 
-    return mc_hist_name_dict, data_hist_name_dict
+    return mc_hist_name_dict, data_hist_name_dict, legend_dict
 
 
 def run_plot(mcfile, datafile, outdir):
+
+    ROOT.gROOT.SetBatch(True)  # Turn off Root Tcanvas display
 
     open_mc_file = ROOT.TFile.Open(mcfile, "READ")
     open_data_file = ROOT.TFile.Open(datafile, "READ")
@@ -64,10 +137,10 @@ def run_plot(mcfile, datafile, outdir):
     total_data_events = open_data_file.Get("hist_total_events").GetBinContent(1)
     print("Total MC/Data", total_mc_events, "/", total_data_events, " events.")
 
-    mc_hist_dict, data_hist_dict = get_mc_data_histograms(open_mc_file, open_data_file)
+    mc_hist_dict, data_hist_dict, legend_dict = get_mc_data_histograms(open_mc_file, open_data_file)
 
     open_plot_file = ROOT.TFile.Open(outdir + "/mc_data_plots.root", "RECREATE")
-    scale_and_plot(mc_hist_dict, data_hist_dict, total_mc_events, total_data_events, outdir)
+    scale_and_plot(mc_hist_dict, data_hist_dict, legend_dict, total_mc_events, total_data_events, outdir)
     open_plot_file.Close()
 
     open_mc_file.Close()

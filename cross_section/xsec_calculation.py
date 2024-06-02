a9ca8339250d8c6778514f72efc9fad322310546
jsensenig <jonsensenig17@gmail.com>
2024-06-02 16:57:41 -0400

    Add differential cross section calculation

--- cross_section/xsec_calculation.py
+++ cross_section/xsec_calculation.py
@@ -24,6 +24,10 @@ class XSecBase:
         self.sigma_factor = argon_molar_mass / (avogadro_constant * liquid_argon_density)
         self.sigma_factor *= 1.e27  # Convert to milli-barn
         self.config = self.configure(config_file=config_file)
+        self.eslice_edges = np.asarray(self.config["eslice_edges"])
+        self.delta_e = bin_width_np(self.eslice_edges)
+
+        self.bethe_bloch = BetheBloch(mass=139.57, charge=1)
 
     @abstractmethod
     def calc_xsec(self, hist_dict):
@@ -38,6 +42,20 @@ class XSecBase:
         Method to implement the error propagation
         """
         pass
+
+    def calculate_incident(self, init_hist, end_hist):
+        """
+        The number incident on a slice is the total start number - the number which ended
+        in all preceding bins.
+        """
+        inc_hist = np.zeros(len(init_hist))
+        for ibin in range(len(self.eslice_edges)-2):
+            for itmp in range(0, ibin+1):
+                inc_hist[ibin+1] += np.flip(init_hist)[itmp]
+            for itmp in range(0, ibin):
+                inc_hist[ibin+1] -= np.flip(end_hist)[itmp]
+
+        return np.flip(inc_hist)
 
     @staticmethod
     def configure(config_file):
@@ -64,9 +82,6 @@ class XSecTotal(XSecBase):
         super().__init__(config_file=config_file)
 
         self.local_config = self.config["XSecTotal"]
-        self.eslices = np.asarray(self.local_config["eslices"])
-        self.delta_e = bin_width_np(self.eslices)
-        self.bethe_bloch = BetheBloch(mass=139.57, charge=1)
         self.geant_total_xsec = {}
 
     def calc_xsec(self, hist_dict):
@@ -77,10 +92,11 @@ class XSecTotal(XSecBase):
         init_hist = hist_dict["init_hist"]
         end_hist = hist_dict["end_hist"]
         int_hist = hist_dict["int_hist"]
+        # inc_hist = self.calculate_incident(init_hist=init_hist, end_hist=end_hist)
         inc_hist = self.calculate_incident(init_hist=init_hist, end_hist=end_hist)
 
         # Get dE/dx as a function fo KE for the center of each bin
-        dedx = np.asarray([self.bethe_bloch.meandEdx(ke) for ke in bin_centers_np(self.eslices)])
+        dedx = np.asarray([self.bethe_bloch.meandEdx(ke) for ke in bin_centers_np(self.eslice_edges)])
 
         # The Eslice cross-section calculation
         xsec = (self.sigma_factor / self.delta_e) * dedx * np.log(inc_hist / (inc_hist - int_hist))
@@ -89,25 +105,6 @@ class XSecTotal(XSecBase):
 
     def propagate_error(self):
         pass
-
-    def calculate_incident(self, init_hist, end_hist):
-        """
-        The number incident on a slice is the total start number - the number which ended
-        in all preceding bins.
-        """
-        inc_hist = np.zeros(len(init_hist))
-        #for b in range(len(self.eslices) - 1):
-        #    # inc_hist[b] = np.sum(np.flip(init_hist)[:b + 1]) - np.sum(np.flip(end_hist)[:b])
-        #    inc_hist[b] = np.sum(np.flip(end_hist)[b:]) - np.sum(np.flip(init_hist)[(b + 1):])
-        #    # inc_hist[b] = np.sum(init_hist[b:]) - np.sum(end_hist[(b + 1):])
-
-        for ibin in range(len(self.eslices)-2):
-            for itmp in range(0, ibin+1):
-                inc_hist[ibin+1] += np.flip(init_hist)[itmp]
-            for itmp in range(0, ibin):
-                inc_hist[ibin+1] -= np.flip(end_hist)[itmp]
-
-        return np.flip(inc_hist)
 
     def load_geant_total_xsec(self, xsec_file):
 
@@ -122,6 +119,10 @@ class XSecDiff(XSecBase):
     """
     The differential cross-section so for the incoming beam particles
     and a single daughter variable, e.g. pi0 Ke, proton angle wrt to beam
+    (dsigma / dX)_i = (1/ndE)_i * (dE/dx)_i * (1 / Delta X)_j * (N^{i,j}_int / N^{i}_inc)
+    i = beam particle energy bin
+    j = daughter variable bin, e.g. E_{pi^0} or cos theta_{pi^0}
+    Note: N^{i,j}_int is a 2D histogram of the beam particle energy and daughter variable
     """
     def __init__(self, config_file):
         super().__init__(config_file=config_file)
@@ -129,7 +130,30 @@ class XSecDiff(XSecBase):
         self.local_config = self.config["XSecDiff"]
 
     def calc_xsec(self, hist_dict):
-        pass
+        # Get the requisite histograms
+        init_hist = hist_dict["init_hist"]
+        end_hist = hist_dict["end_hist"]
+        int_hist = hist_dict["int_hist"]
+
+        assert int_hist.ndim == 2, f"Interacting histogram must be 2D but its {int_hist.ndim}D!"
+        assert int_hist.shape[0] == end_hist.shape, f"Interacting histogram axis 0 must match Initial/End histogram shape!"
+
+        inc_hist = self.calculate_incident(init_hist=init_hist, end_hist=end_hist)
+
+        # Get dE/dx as a function fo KE for the center of each bin
+        dedx = np.asarray([self.bethe_bloch.meandEdx(ke) for ke in bin_centers_np(self.eslice_edges)])
+
+        # The Eslice cross-section calculation
+        total_xsec_prefactor = (self.sigma_factor / self.delta_e) * dedx
+
+        # The cross section result is going to be 2D, the same shape as the interacting histogram
+        xsec_array = np.zeros_like(int_hist)
+
+        # Loop over the y-axis of int hist, assumed to be dX
+        for j in range(init_hist.shape[1]):
+            xsec_array[:, j] = total_xsec_prefactor * (1. / bin_width_np(init_hist[0, :])) * (int_hist[:, j] / inc_hist)
+
+        return xsec_array
 
     def propagate_error(self):
         pass
@@ -139,6 +163,11 @@ class XSecDoubleDiff(XSecBase):
     """
     The differential cross-section so for the incoming beam particles
     and two daughter variables, e.g. pi0 Ke, pi0 angle wrt to beam
+    (d^2sigma / dX dY)_i = (1/ndE)_i * (dE/dx)_i * (1 / Delta X)_j * (1 / Delta Y)_k * (N^{i,j,k}_int / N^{i}_inc)
+    i = beam particle energy bin
+    j = daughter variable bin, e.g. E_{pi^0} or cos theta_{pi^0}
+    k = daughter variable bin, e.g. E_{pi^0} or cos theta_{pi^0}
+    Note: N^{i,j,k}_int is a 3D histogram of the beam particle energy and 2 daughter variables
     """
     def __init__(self, config_file):
         super().__init__(config_file=config_file)
@@ -147,7 +176,13 @@ class XSecDoubleDiff(XSecBase):
         self.geant_xsec_dict = {}
 
     def calc_xsec(self, hist_dict):
-        pass
+        # Get the requisite histograms
+        init_hist = hist_dict["init_hist"]
+        end_hist = hist_dict["end_hist"]
+        int_hist = hist_dict["int_hist"]
+
+        assert int_hist.ndim == 3, f"Interacting histogram must be 3D but its {int_hist.ndim}D!"
+        assert int_hist.shape[0] == end_hist.shape, f"Interacting histogram axis 0 must match Initial/End histogram shape!"
 
     def propagate_error(self):
         pass

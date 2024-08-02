e8556ccd676c4c00c6c7817f2ecc35a3965ca5e4
jsensenig <jonsensenig17@gmail.com>
2024-08-01 22:56:20 -0400

    Add handling of data/MC and remove unused code

--- run_cex_selection_no_threads.py
+++ run_cex_selection_no_threads.py
@@ -1,8 +1,6 @@
 from timeit import default_timer as timer
 from cex_analysis.event_handler import EventHandler
-from cex_analysis.histograms import Histogram
 import cex_analysis.efficiency_data as eff_data
-import cex_analysis.histogram_data as hdata
 import numpy as np
 import uproot
 import time
@@ -19,55 +17,6 @@ def open_file(file_name, tree_dir):
         print("Could not find file", file_name)
         return
     return tree
-
-
-def merge_hist_maps(config, hist_maps):
-    """
-    Here we want to,
-    1. Loop over each type of histogram
-    2. Collect histograms from all threads for a given histogram type
-    3. Add all histograms of the same name (they should be the same histogram
-       just from a different thread)
-    4. Write the summed histogram to file.
-    :param config: Config to set up the
-    :param hist_maps:
-    :return:
-    """
-    # Set to batch mode so no windows pop up
-    # ROOT.gROOT.SetBatch(True)
-
-    # Open file to which we write results
-    # f = ROOT.TFile("result_file.root", "RECREATE")
-    #
-    # hclass = Histogram(config)
-    # # hist_maps = [[Thread 0],..., [Thread N]]
-    # type_list = ["stack", "hist", "efficiency"]
-    # for t in type_list:
-    #     hist_type_list = []
-    #     for res in hist_maps: # results from each thread
-    #         hist_type_list += hdata.get_hist_type_list(res, t)
-    #     if len(hist_type_list) < 1:
-    #         continue
-    #     # Only returns unique names as a set
-    #     hist_names = hdata.get_hist_name_list(hist_type_list)
-    #     for name in hist_names:
-    #         hlist = hdata.get_select_hist_name_list(hist_type_list, name)
-    #         merged_hist = None
-    #         if t == "efficiency":
-    #             merged_hist = hclass.merge_efficiency_list(hlist)
-    #         elif t == "hist":
-    #             merged_hist = hclass.merge_hist_list(hlist)
-    #         elif t == "stack":
-    #             merged_hist = hclass.merge_stack_list(hlist)
-    #         else:
-    #             print("Unknown histogram type! ", t)
-    #         if merged_hist is not None:
-    #             merged_hist.Write(t + "_" + name)
-    #             c = ROOT.TCanvas()
-    #             merged_hist.Draw()
-    #             c.SaveAs("hist_figs/" + t + "_" + name + ".pdf")
-    #
-    # f.Close()
 
 
 def calculate_efficiency(selected_true, selected_total, true_count_list):
@@ -89,7 +38,6 @@ def save_results(results):
 
     hist_map, event_mask, cut_signal_selected, cut_total_selected, signal_total, events, beam_events = results
 
-    # merge_hist_maps(config, result_hist_list)
     calculate_efficiency(cut_signal_selected, cut_total_selected, signal_total)
 
     *_, num_last_cut = cut_signal_selected.items()
@@ -170,34 +118,42 @@ def configure(config_file):
         return json.load(cfg)
 
 
-if __name__ == "__main__":
+def get_branches(is_mc):
 
-    tree_name = "pionana/beamana;17"
     branches = ["event", "reco_daughter_PFP_true_byHits_startZ", "reco_daughter_PFP_true_byHits_PDG", "reco_beam_passes_beam_cuts",
-                "reco_daughter_PFP_true_byHits_parPDG", "true_beam_daughter_startP", "true_beam_daughter_PDG",
+                "reco_daughter_PFP_true_byHits_parPDG",
                 "reco_beam_true_byHits_PDG", "reco_daughter_allShower_energy", "reco_daughter_PFP_trackScore_collection",
                 "reco_daughter_allTrack_Chi2_proton", "reco_daughter_allTrack_Chi2_ndof", "beam_inst_TOF",
-                "true_daughter_nPiMinus", "true_daughter_nPiPlus", "true_daughter_nPi0", "true_daughter_nProton",
-                "true_daughter_nNeutron","true_beam_PDG","true_beam_endProcess","true_beam_endZ","true_beam_endP","true_beam_endPx",
-                "true_beam_endPy", "true_beam_endPz", "reco_daughter_PFP_michelScore_collection", "reco_beam_calo_startX",
-                "reco_beam_calo_startY", "reco_beam_calo_startZ", "reco_beam_calo_endX", "reco_beam_calo_endY",
-                "reco_beam_calo_endZ", "true_beam_daughter_startPx", "true_beam_daughter_startPy", "true_beam_daughter_startPz",
-                "reco_beam_true_byHits_endProcess", "reco_daughter_PFP_nHits", "beam_inst_P", "reco_beam_vertex_michel_score",
-                "reco_beam_vertex_nHits", "reco_daughter_allTrack_dEdX_SCE", "reco_beam_endX", "reco_beam_endY",
-                "reco_beam_endZ"]
-
-    branches += ["true_beam_Pi0_decay_startPx", "true_beam_Pi0_decay_startPy", "true_beam_Pi0_decay_startPz",
-                 "true_beam_Pi0_decay_PDG", "true_beam_Pi0_decay_ID", "true_beam_Pi0_decay_startP",
-                 "true_beam_Pi0_decay_startX", "true_beam_Pi0_decay_startY", "true_beam_Pi0_decay_startZ",
-                 "true_beam_endX", "true_beam_endY", "true_beam_endZ", "reco_beam_trackEndDirX",
-                 "reco_beam_trackEndDirY", "reco_beam_trackEndDirZ", "true_beam_interactingEnergy",
-                 "true_beam_incidentEnergies", "true_beam_traj_Z", "true_beam_traj_KE"]#, "dEdX_truncated_mean"]
-
-    branches += ["reco_beam_calo_startDirX", "reco_beam_calo_startDirY", "reco_beam_calo_startDirZ",
-                 "reco_beam_calo_endDirX", "reco_beam_calo_endDirY", "reco_beam_calo_endDirZ",
-                 "true_beam_slices"]#, "true_beam_traj_incidentEnergies", "true_beam_traj_interacting_Energy"]
+                "reco_daughter_PFP_michelScore_collection", "reco_beam_calo_startX",  "reco_beam_calo_startY",
+                "reco_beam_calo_startZ", "reco_beam_calo_endX", "reco_beam_calo_endY", "reco_beam_calo_endZ",
+                "reco_beam_true_byHits_endProcess", "reco_daughter_PFP_nHits", "beam_inst_P",
+                "reco_beam_vertex_michel_score", "reco_beam_vertex_nHits", "reco_daughter_allTrack_dEdX_SCE",
+                "reco_beam_endX", "reco_beam_endY", "reco_beam_endZ", "reco_beam_trackEndDirX", "reco_beam_trackEndDirY",
+                "reco_beam_trackEndDirZ", "dEdX_truncated_mean", "reco_beam_calo_startDirX", "reco_beam_calo_startDirY",
+                "reco_beam_calo_startDirZ", "reco_beam_calo_endDirX", "reco_beam_calo_endDirY", "reco_beam_calo_endDirZ"]
 
     branches += ["reco_all_spacePts_X", "reco_all_spacePts_Y", "reco_all_spacePts_Z", "reco_all_spacePts_Integral"]
+
+    #################
+    # Truth variables
+    if is_mc:
+        branches += ["true_beam_daughter_startP", "true_beam_daughter_PDG", "true_daughter_nPiMinus", "true_daughter_nPiPlus",
+                     "true_daughter_nPi0", "true_daughter_nProton", "true_daughter_nNeutron","true_beam_PDG",
+                     "true_beam_endProcess","true_beam_endZ","true_beam_endP","true_beam_endPx", "true_beam_endPy",
+                     "true_beam_endPz", "true_beam_daughter_startPx", "true_beam_daughter_startPy", "true_beam_daughter_startPz"]
+
+        branches += ["true_beam_interactingEnergy", "true_beam_incidentEnergies", "true_beam_traj_Z", "true_beam_traj_KE"]
+
+        branches += ["true_beam_Pi0_decay_startPx", "true_beam_Pi0_decay_startPy", "true_beam_Pi0_decay_startPz",
+                     "true_beam_Pi0_decay_PDG", "true_beam_Pi0_decay_ID", "true_beam_Pi0_decay_startP",
+                     "true_beam_Pi0_decay_startX", "true_beam_Pi0_decay_startY", "true_beam_Pi0_decay_startZ",
+                     "true_beam_endX", "true_beam_endY", "true_beam_endZ"]
+
+    return branches
+
+
+if __name__ == "__main__":
+
 
     # Provide a text file with one file per line
     files = "/Users/jsen/tmp/pion_qe/2gev_single_particle_sample/ana_alldaughter_files.txt"
@@ -212,6 +168,9 @@ if __name__ == "__main__":
     # Get main configuration
     cfg_file = "config/main.json"
     config = configure(cfg_file)
+
+    tree_name = "pionana/beamana;17"
+    branches = get_branches(is_mc=config["is_mc"])
 
     # Start the analysis threads
     print("Starting threads")

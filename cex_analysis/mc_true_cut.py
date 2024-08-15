2eac00ab11139b82ea671971f646236503fc9da2
jsensenig <jonsensenig17@gmail.com>
2024-08-15 15:00:02 -0400

    Add cut for true beam interaction process

---
+++ cex_analysis/mc_true_cut.py
@@ -0,0 +1,67 @@
+from cex_analysis.event_selection_base import EventSelectionBase
+from cex_analysis.true_process import TrueProcess
+import awkward as ak
+
+
+class MCTrueCut(EventSelectionBase):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.cut_name = "MCTrueCut"
+        self.config = config
+        self.reco_daughter_pdf = self.config["reco_beam_pdg"]
+
+        if self.config["is_mc"]:
+            print("Processing data, please do not use this MC cut!")
+            raise AssertionError
+
+        # Configure class
+        self.local_config, self.local_hist_config = super().configure(config_file=self.config[self.cut_name]["config_file"],
+                                                                      cut_name=self.cut_name)
+        self.optimize = self.local_config["optimize_cut"]
+
+        true_process = TrueProcess()
+        self.signal = self.config["true_signal"]
+
+        if self.signal not in true_process.get_process_list():
+            print("Unknown signal", self.signal)
+            raise ValueError
+
+    def selection(self, events, hists, optimizing=False):
+
+        # First we configure the histograms we want to make
+        if not optimizing:
+            hists.configure_hists(self.local_hist_config)
+
+        # Plot the variable before making cut
+        if not optimizing:
+            self.plot_particles_base(events=events, pdg=events[self.reco_daughter_pdf], precut=True, hists=hists)
+
+        selected_mask = ak.to_numpy(events[self.signal])
+
+        # Plot the variable before after cut
+        if not optimizing:
+            self.plot_particles_base(events=events[selected_mask], pdg=events[self.reco_daughter_pdf, selected_mask],
+                                     precut=False, hists=hists)
+            # Plot the efficiency
+            self.efficiency(total_events=events, passed_events=events[selected_mask], cut=self.cut_name, hists=hists)
+
+        # Return event selection mask
+        return selected_mask
+
+    def plot_particles_base(self, events, pdg, precut, hists):
+        for idx, plot in enumerate(self.local_hist_config):
+            hists.plot_process_stack(x=events, idx=idx, variable=plot, precut=precut)
+            hists.plot_particles_stack(x=events[plot], x_pdg=pdg, idx=idx, precut=precut)
+            hists.plot_particles(x=events[plot], idx=idx, precut=precut)
+
+    def efficiency(self, total_events, passed_events, cut, hists):
+        for idx, plot in enumerate(self.local_hist_config):
+            hists.plot_efficiency(xtotal=total_events[plot], xpassed=passed_events[plot], idx=idx)
+
+    def cut_optimization(self):
+        pass
+
+    def get_cut_doc(self):
+        doc_string = "Cut on MC interaction process"
+        return doc_string

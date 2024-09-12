Correct masking of events which fall outside of defined range

unfolding/unfold_events.py
@@ -93,24 +93,24 @@ class Unfold:
 
         if self.is_training:
             print("Start Training")
-            true_nd_binned, true_weights, true_nd_hist, true_nd_hist_cov, true_hist_sparse, self.remap_evts.true_map\
+            true_nd_binned, true_weights, true_nd_hist, true_nd_hist_cov, true_hist_sparse, self.remap_evts.true_map, true_evt_mask \
                 = self.remap_evts.remap_events(var_list=true_var_list,
                                                bin_list=self.true_bin_array,
                                                event_weights=true_weight_list)
 
-            reco_nd_binned, reco_weights, reco_nd_hist, reco_nd_hist_cov, reco_hist_sparse, self.remap_evts.reco_map \
+            reco_nd_binned, reco_weights, reco_nd_hist, reco_nd_hist_cov, reco_hist_sparse, self.remap_evts.reco_map, reco_evt_mask \
                 = self.remap_evts.remap_events(var_list=reco_var_list,
                                                bin_list=self.true_bin_array,
                                                event_weights=reco_weight_list)
 
             # Only used for efficiency calculation
-            _, _, signal_nd_hist, _, signal_hist_sparse, _ = self.remap_evts.remap_events(var_list=signal_var_list,
+            _, _, signal_nd_hist, _, signal_hist_sparse, _, _ = self.remap_evts.remap_events(var_list=signal_var_list,
                                                                                           bin_list=self.true_bin_array,
                                                                                           event_weights=signal_weight_list,
                                                                                           is_true_reco=False, is_data=False)
             sel_signal = [var[selected_signal_mask] for var in signal_var_list]
             sel_weight = signal_weight_list[selected_signal_mask]
-            _, _, selected_signal_nd_hist, _, _, _ = self.remap_evts.remap_events(var_list=sel_signal,                  
+            _, _, selected_signal_nd_hist, _, _, _, _ = self.remap_evts.remap_events(var_list=sel_signal,
                                                                                   bin_list=self.true_bin_array,
                                                                                   event_weights=sel_weight,
                                                                                   is_true_reco=False, is_data=False)
@@ -120,15 +120,16 @@ class Unfold:
 
             self.truth_nbins_sparse, self.reco_nbins_sparse = len(true_hist_sparse), len(reco_hist_sparse)
 
-            self.create_response_matrix(reco_events=self.remap_evts.reco_map[reco_nd_binned].astype('d'),
-                                        true_events=self.remap_evts.true_map[true_nd_binned].astype('d'),
-                                        reco_weights=reco_weights.astype('d'))
+            response_mask = true_evt_mask & reco_evt_mask
+            self.create_response_matrix(reco_events=self.remap_evts.reco_map[reco_nd_binned[response_mask]].astype('d'),
+                                        true_events=self.remap_evts.true_map[true_nd_binned[response_mask]].astype('d'),
+                                        reco_weights=reco_weights[response_mask].astype('d'))
 
         if self.show_plots:
             self.plot_response_matrix(response_matrix=self.response)
 
         # Set data
-        _, _, _, _, data_hist_sparse, _ = self.remap_evts.remap_events(var_list=reco_var_list,
+        _, _, _, _, data_hist_sparse, _, _ = self.remap_evts.remap_events(var_list=reco_var_list,
                                                                        bin_list=self.true_bin_array,
                                                                        event_weights=data_weight_list,
                                                                        is_true_reco=False, is_data=True)
@@ -416,11 +417,11 @@ class Unfold:
             nbins, bin_range = self.config["truth_bins"]["nbins"], self.config["truth_bins"]["limits"]
             self.truth_ndim = len(nbins)
             true_array = [np.linspace(limits[0], limits[1], bin + 1) for bin, limits in zip(nbins, bin_range)]
-            true_array = [np.concatenate(([-1000], tarr, [limits[1] + 1000])) for tarr, limits in zip(true_array, bin_range)]
+            true_array = [np.concatenate(([-1.e6], tarr, [limits[1] + 1.e6])) for tarr, limits in zip(true_array, bin_range)]
             nbins, bin_range = self.config["reco_bins"]["nbins"], self.config["reco_bins"]["limits"]
             self.reco_ndim = len(nbins)
             reco_array = [np.linspace(limits[0], limits[1], bin + 1) for bin, limits in zip(nbins, bin_range)]
-            reco_array = [np.concatenate(([-1000], tarr, [limits[1] + 1000])) for tarr, limits in zip(reco_array, bin_range)]
+            reco_array = [np.concatenate(([-1.e6], tarr, [limits[1] + 1.e6])) for tarr, limits in zip(reco_array, bin_range)]
 
         return true_array, reco_array
 

unfolding/unfold_remapping.py
@@ -25,10 +25,11 @@ class Remapping:
         self.true_total_bins = np.prod([len(d) - 1 for d in bin_list])
         num_bin_list = [len(d) - 1 for d in bin_list]
 
-        nd_binned, weights, nd_hist, nd_hist_err, nd_hist_cov = self.map_meas_to_bin_space(corr_var_list=var_list,
+        nd_binned, weights, nd_hist, nd_hist_err, nd_hist_cov, evt_mask = self.map_meas_to_bin_space(corr_var_list=var_list,
                                                                                            bin_list=bin_list,
                                                                                            total_bins=self.true_total_bins,
                                                                                            evt_weights=event_weights,
+                                                                                           is_data=is_data,
                                                                                            debug=self.debug)
 
         print("Total Bins:", self.true_total_bins)
@@ -56,7 +57,7 @@ class Remapping:
 
         print("Map:", np.count_nonzero(nd_map))
 
-        return nd_binned, weights, nd_hist, nd_hist_cov, hist_sparse, nd_map
+        return nd_binned, weights, nd_hist, nd_hist_cov, hist_sparse, nd_map, evt_mask
 
     def subtract_background(self, nd_hist, nd_hist_err):
         """
@@ -142,16 +143,18 @@ class Remapping:
         # 3D (l,m,n) -> (l*m*n) + (m*n) + n
         self.true_total_bins = np.prod([len(d) - 1 for d in bin_list])
 
-        true_nd_binned, true_weights, true_nd_hist, true_nd_hist_err, true_nd_hist_cov = self.map_meas_to_bin_space(corr_var_list=true_list,
+        true_nd_binned, true_weights, true_nd_hist, true_nd_hist_err, true_nd_hist_cov, true_evt_mask = self.map_meas_to_bin_space(corr_var_list=true_list,
                                                                                                bin_list=bin_list,
                                                                                                total_bins=self.true_total_bins,
                                                                                                evt_weights=true_event_weights,
+                                                                                               is_data=False,
                                                                                                debug=self.debug)
 
-        reco_nd_binned, reco_weights, reco_nd_hist, reco_nd_hist_err, reco_nd_hist_cov = self.map_meas_to_bin_space(corr_var_list=reco_list,
+        reco_nd_binned, reco_weights, reco_nd_hist, reco_nd_hist_err, reco_nd_hist_cov, reco_evt_mask = self.map_meas_to_bin_space(corr_var_list=reco_list,
                                                                                                bin_list=bin_list,
                                                                                                total_bins=self.true_total_bins,
                                                                                                evt_weights=reco_event_weights,
+                                                                                               is_data=False,
                                                                                                debug=self.debug)
 
         print("Total Bins:", self.true_total_bins)
@@ -171,16 +174,17 @@ class Remapping:
         print("Meas Map:", np.count_nonzero(self.reco_map))
 
         return (true_nd_binned, reco_nd_binned), (true_weights, reco_weights), (true_nd_hist, reco_nd_hist), (true_nd_hist_cov, reco_nd_hist_cov), \
-               (true_hist_sparse, reco_hist_err_sparse)
+               (true_hist_sparse, reco_hist_err_sparse), (true_evt_mask, reco_evt_mask)
 
     def remap_data_events(self, data_list, bin_list, data_weights):
 
         self.reco_total_bins = np.prod([len(d)-1 for d in bin_list])
 
-        data_nd_binned, data_weight, data_nd_hist, data_nd_hist_err, data_nd_hist_cov = self.map_meas_to_bin_space(corr_var_list=data_list,
+        data_nd_binned, data_weight, data_nd_hist, data_nd_hist_err, data_nd_hist_cov, evt_mask = self.map_meas_to_bin_space(corr_var_list=data_list,
                                                                                                bin_list=bin_list,
                                                                                                total_bins=self.reco_total_bins,
                                                                                                evt_weights=data_weights,
+                                                                                               is_data=True,
                                                                                                debug=self.debug)
 
         # Data mapping to 1D
@@ -188,7 +192,7 @@ class Remapping:
                                                                           map_nd1d=self.reco_map)
         print("Sparse Data nbins:", len(data_hist_sparse))
 
-        return data_nd_binned, data_weight, data_nd_hist, data_nd_hist_cov, data_hist_sparse, data_hist_err_sparse
+        return data_nd_binned, data_weight, data_nd_hist, data_nd_hist_cov, data_hist_sparse, data_hist_err_sparse, evt_mask
 
     @staticmethod
     def calculate_efficiency(full_signal, full_selected):
@@ -224,7 +228,7 @@ class Remapping:
         return unfolded_data_corrected, unfolded_data_cov_corrected
 
     @staticmethod
-    def map_meas_to_bin_space(corr_var_list, bin_list, total_bins, evt_weights, debug=False):
+    def map_meas_to_bin_space(corr_var_list, bin_list, total_bins, evt_weights, is_data, debug=False):
         """
         Convert list of correlated variables to single ndim variable
         """
@@ -232,7 +236,7 @@ class Remapping:
         mask_events = np.ones(shape=corr_var_list[0].shape, dtype=bool)
         print("Pre", np.count_nonzero(mask_events))
         for cvar, bins in zip(corr_var_list, bin_list): # NOTE remove masking
-            mask_events &= (cvar >= bins[0]) & (cvar < bins[-1])
+            mask_events &= (cvar >= bins[1]) & (cvar < bins[-2])
         print("Post", np.count_nonzero(mask_events))
 
         bin_lens = [len(d)-1 for d in bin_list] # NOTE changed from len() -1
@@ -243,7 +247,7 @@ class Remapping:
             arr, bins, shift = arr_bin
             nbins = len(bins) - 1
             # Mapping from measured space (usually energy) to bin space
-            n_binned = np.digitize(x=arr[mask_events], bins=bins, right=False) - 1
+            n_binned = np.digitize(x=arr, bins=bins, right=False) - 1
             if debug: print("Unique bins:", np.unique(n_binned))
             if debug: print("(n_binned >= 0) & (n_binned <= (", nbins-1, ")") # was >= 1 and <= 0 # NOTE remove cut
             n_binned = n_binned[(n_binned >= 0) & (n_binned <= nbins-1)]  # ignore under/over flow bins, 0/n+1 respectively
@@ -259,12 +263,17 @@ class Remapping:
         if debug: print("Unique Bins:", np.unique(ravelled_idx))
 
         # Create the histogram with event weighting and calculate errors
-        num_nd, _ = np.histogram(ravelled_idx, bins=total_bins, range=(0, total_bins-1), weights=evt_weights[mask_events])
+        if is_data:
+            num_nd, _ = np.histogram(ravelled_idx[mask_events], bins=total_bins, range=(0, total_bins - 1),
+                                     weights=evt_weights[mask_events])
+        else:
+            num_nd, _ = np.histogram(ravelled_idx, bins=total_bins, range=(0, total_bins - 1), weights=evt_weights)
+
         num_nd_err, _ = np.histogram(ravelled_idx, bins=total_bins, range=(0, total_bins-1),
-                                     weights=evt_weights[mask_events] * evt_weights[mask_events])
+                                     weights=evt_weights * evt_weights)
         num_nd_vcov = np.diag(num_nd_err)
 
-        return ravelled_idx, evt_weights[mask_events], num_nd, np.sqrt(num_nd_err), num_nd_vcov
+        return ravelled_idx, evt_weights, num_nd, np.sqrt(num_nd_err), num_nd_vcov, mask_events
 
     @staticmethod
     def map_nd_to_1d(num_nd, num_nd_err, total_bins):